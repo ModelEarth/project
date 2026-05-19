53ea85af2fc5e22da8103d274cb168cac3f40168
Neha Bais Thakur <nehab1229@gmail.com>
2026-05-19 13:13:57 -0700

    Move cost range chart above generation mix and risk charts

--- js/event-hub.js
+++ js/event-hub.js
@@ -295,8 +295,10 @@
 
     // Chart containers
     html += '<div class="hub-charts-row">';
+    html += '<div class="hub-chart-wrap"><div class="hub-chart-label" style="text-align:center">Cost range by phase (USD)</div><div id="hubCostChart" style="height:220px;"></div></div>';
+    html += '</div>';
+    html += '<div class="hub-charts-row">';
     html += '<div class="hub-chart-wrap"><div class="hub-chart-label">Generation mix by scenario</div><div id="hubScenarioChart" style="height:220px;"></div></div>';
-    html += '<div class="hub-chart-wrap"><div class="hub-chart-label">Cost range by phase (USD)</div><div id="hubCostChart" style="height:220px;"></div></div>';
     html += '<div class="hub-chart-wrap"><div class="hub-chart-label">Risk dimension confidence</div><div id="hubRiskChart" style="height:220px;"></div></div>';
     html += '</div>';
 
